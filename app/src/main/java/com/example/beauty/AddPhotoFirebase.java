badc6a366a66b170a3e79ce4997c0ea637e826a1
Глушко Володимир <mr.vproen7shchsgjg@gmail.com>
2021-09-30 21:45:10 +0300

    Сохранение мелких изминений

--- app/src/main/java/com/example/beauty/AddPhotoFirebase.java
+++ app/src/main/java/com/example/beauty/AddPhotoFirebase.java
@@ -1,9 +1,15 @@
 package com.example.beauty;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.beauty.Menu.HomeActivity;
+import com.example.beauty.StartMenu.RegisterActivity;
 
 public class AddPhotoFirebase extends AppCompatActivity {
 
@@ -13,11 +19,28 @@ public class AddPhotoFirebase extends AppCompatActivity {
         setContentView(R.layout.activity_add_photo_firebase);
     }
 
+    public void Back(View view) {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     public void GoGallery(View view) {
+
+        Intent intentGallery = new Intent();
+        intentGallery.setType("image/");
+        intentGallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentGallery, 1);
 
     }
 
     public void PushInfoPost(View view) {
 
     }
+
+
+
 }
