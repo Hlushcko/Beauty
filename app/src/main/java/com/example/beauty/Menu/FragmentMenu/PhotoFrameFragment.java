4020583d62dabca261c2085a96a3facbf1fb9c84
VovaHl <mr.vproen7shchsgjg@gmail.com>
2022-02-22 21:10:02 +0200

    Теперь фото отображаются в фрагментах.

--- app/src/main/java/com/example/beauty/Menu/FragmentMenu/PhotoFrameFragment.java
+++ app/src/main/java/com/example/beauty/Menu/FragmentMenu/PhotoFrameFragment.java
@@ -1,19 +1,14 @@
 package com.example.beauty.Menu.FragmentMenu;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.databinding.BindingAdapter;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 import com.example.beauty.R;
 
 
@@ -21,9 +16,7 @@ public class PhotoFrameFragment extends Fragment{
 
     private static final String KEY_URL_PHOTO = "Photo";
 
-    ImageView imagePhoto;
-    View view;
-
+    private String uri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,23 +25,28 @@ public class PhotoFrameFragment extends Fragment{
         Bundle bundle = this.getArguments();
 
         assert bundle != null;
-        String uri = bundle.getString(KEY_URL_PHOTO);
-
-        view = inflater.inflate(R.layout.fragment_photo_frame, container, false);
-        imagePhoto = (ImageView) view.findViewById(R.id.PhotoPeople);
-
-        TextView text1 = view.findViewById(R.id.TextView1);
-        text1.setText(uri);
-
-//        Glide
-//                .with(imagePhoto.getContext())
-//                .load(uri)
-//                .error(R.drawable.ic_launcher_foreground)
-//                .centerCrop()
-//                .into(imagePhoto);
-
+        uri = bundle.getString(KEY_URL_PHOTO);
 
         return inflater.inflate(R.layout.fragment_photo_frame, container, false);
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        View view = getView();
+        if(view != null){
+            ImageView image = (ImageView) view.findViewById(R.id.PhotoPeople);
+
+            Glide
+                .with(this)
+                .load(uri)
+                .error(R.drawable.ic_launcher_foreground)
+                .centerCrop()
+                .into(image);
+        }
+
+    }
+
 
 }
